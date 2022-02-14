dfd9b65131298a8fa8651f4dd2ecb6a5b8e9ae88
Fateme Emadi <fatemehemadi2021@gmail.com>
2022-02-14 21:17:57 +0330

    php calculator

---
+++ calculator.php
@@ -0,0 +1,39 @@
+<html>
+    <head>
+
+    </head>
+<body>
+<?php 
+$number1=45;
+$number2=37;
+$operator="+";
+print("number1=".$number1);
+print("<br>");
+print("number2=".$number2);
+print("<br>");
+//if(isset($_GET["Submit"])){
+   // $operator=$_GET["operator"];
+    if($operator=="+"){
+     $result=$number1+$number2;
+     print("result=".$result);
+    }
+    else if($operator=="-"){
+     $result=$number1-$number2;  
+     print("result=".$result);
+    }
+    else if($operator=="*"){
+     $result=$number1*$number2; 
+     print("result=".$result);
+       }
+    else if($operator=="/"){
+        $result=$number1/$number2;
+     print("result=".$result);
+    }
+//}
+//}
+
+?>
+
+
+</body>
+</html>
